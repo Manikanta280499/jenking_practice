82b1937167136d968fc846f94febc0eecef12ab0
mani <manikanta280499@gmail.com>
2024-08-02 11:21:23 +0530

    first commit

---
+++ src/test/java/package1/Frames.java
@@ -0,0 +1,30 @@
+package package1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Frames {
+	
+	@Test
+	public void framesPractice() {
+		WebDriver w = new ChromeDriver();
+		w.get("https://the-internet.herokuapp.com/nested_frames");
+		w.switchTo().frame(1);
+		System.out.println(w.findElement(By.cssSelector("body")).getText());
+		w.switchTo().defaultContent();
+		w.switchTo().frame(w.findElement(By.cssSelector("[name='frame-top']"))).switchTo().frame(2);
+		System.out.println(w.findElement(By.cssSelector("body")).getText());
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		w.quit();
+	}
+	
+	
+
+}
